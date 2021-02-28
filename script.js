59ad14ff42b3aeaab5b18f9927ce92adeaced4ae
Alex Hang <alexandruhang@yahoo.com>
2021-02-28 09:37:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,10 @@ const roomName = 'observable-' + roomHash;
 const configuration = {
     'iceServers': [
     {
-      'url': 'stun:stun.l.google.com:19302'
+      'url': 'numb.viagenie.ca',
+      'credential':'iHelpRobotics',
+      'username': 'alexandruhang@yahoo.com'
     },
-    {
-      'url': 'turn:192.158.29.39:3478?transport=udp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    },
-    {
-      'url': 'turn:192.158.29.39:3478?transport=tcp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    }
   ]
 };
 let room;
