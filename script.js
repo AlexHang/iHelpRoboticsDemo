cc5e9c268f5c249d9331333be8597cb2c5dc3215
Alex Hang <alexandruhang@yahoo.com>
2021-02-28 09:03:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,21 @@ const drone = new ScaleDrone('rNJKWINZW3VIAWU2');
 const roomName = 'observable-' + roomHash;
 //const roomName = 'observable-testPHR';
 const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
-  }]
+    'iceServers': [
+    {
+      'url': 'stun:stun.l.google.com:19302'
+    },
+    {
+      'url': 'turn:192.158.29.39:3478?transport=udp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    },
+    {
+      'url': 'turn:192.158.29.39:3478?transport=tcp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    }
+  ]
 };
 let room;
 let pc;
